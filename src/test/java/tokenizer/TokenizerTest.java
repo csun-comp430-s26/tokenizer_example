ae402f31905a8eae81cd13929ea8ba512254e143
Kyle Dewey <kyle.dewey@csun.edu>
2026-02-23 15:46:25 -0800

    tests passing, still can improve coverage

--- src/test/java/tokenizer/TokenizerTest.java
+++ src/test/java/tokenizer/TokenizerTest.java
@@ -14,6 +14,12 @@ public class TokenizerTest {
     }
 
     @Test
+    public void testOnlyWhitespace() throws TokenizerException {
+        assertEquals(Arrays.asList(),
+                     Tokenizer.tokenize("   "));
+    }
+
+    @Test
     public void testIdentifier() throws TokenizerException {
         assertEquals(Arrays.asList(new IdentifierToken("x")),
                      Tokenizer.tokenize("x"));
